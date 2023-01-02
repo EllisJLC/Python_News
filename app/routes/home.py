68d057c4f452b04d1dba36d8a0ed86b57e8c0387
EllisJLC <jia.liang.chen@ryerson.ca>
2023-01-02 02:29:36 -0500

    Started routes

---
+++ app/routes/home.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+bp = Blueprint('home', __name__, url_prefix='/') # Blueprint is used to aggregate routes
+
+@bp.route('/')
+def index():
+  return render_template('homepage.html') # Use render_template to render a template upon navigating to the page.
+
+@bp.route('/login')
+def login():
+  return render_template('login.html')
